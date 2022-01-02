a664443c03bd9c3ec006958c4383d71a758c4408
Denis <wolf-den@tut.by>
2022-01-02 13:44:40 +0300

    composition api setup and ref

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <div class="card">
       <h1>Vue Composition Api</h1>
+      <small>data, computed, methods, watch</small>
       <hr>
       <p>Название: <strong>{{ name }}</strong></p>
       <p>Версия: <strong>{{ version }}</strong></p>
@@ -12,18 +13,28 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
-  data() {
+  setup() {
+    const name = ref('VueJS')
+    const version = ref(3)
+
     return {
-      name: 'VueJS',
-      version: 3
-    }
-  },
-  methods: {
-    changeInfo() {
-      this.name = 'Vue JS!'
-      this.version = 4
+      name,
+      version
     }
   }
+  // data() {
+  //   return {
+  //     name: 'VueJS',
+  //     version: 3
+  //   }
+  // },
+  // methods: {
+  //   changeInfo() {
+  //     this.name = 'Vue JS!'
+  //     this.version = 4
+  //   }
+  // }
 }
 </script>
