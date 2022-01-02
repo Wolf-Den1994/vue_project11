3a4e0791939f220499daec3d3c836255e2a2661b
Denis <wolf-den@tut.by>
2022-01-02 16:24:13 +0300

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,22 @@
 <template>
-  <div class="container">
-    <div class="card">
-      <h1>Vue Composition Api</h1>
-      <small>data, computed, methods, watch</small>
-      <hr>
-
-      <div class="form-control">
-        <input type="text" ref="textInput">
-        <input type="text" v-model="firstName">
-      </div>
-
-      <button class="btn" @click="change">Изменить</button>
-    </div>
-
-    <framework-info
-      @change-version="changeVersion"
-      class="text-from-app"
-    >
-      <template #footer>
-        <p>this is footer</p>
-      </template>
-    </framework-info>
+  <header class="navbar">
+    <strong>Vue Composition Api</strong>
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/">Главная</router-link>
+      </li>
+      <li>
+        <router-link to="/reusable">Переиспользование</router-link>
+      </li>
+    </ul>
+  </header>
+  <div class="container with-nav">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import {
-  ref,
-  reactive,
-  toRefs,
-  isRef,
-  isReactive,
-  computed,
-  watch,
-  provide,
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted,
-} from 'vue'
-import FrameworkInfo from "@/FrameworkInfo";
 export default {
-  setup() {
-    const name = ref('VueJS')
-    const version = ref(3)
-    const textInput = ref(null)
-    const firstName = ref('')
 
-    // console.log(isRef(name)) //true
-    // console.log(isRef(version.value)) //false
-
-    const framework = reactive({
-      name: 'VueJS',
-      version: 3
-    })
-
-    console.log('created')
-
-    onBeforeMount(() => {
-      console.log('onBeforeMount')
-    })
-
-    onMounted(() => {
-      console.log('onMounted')
-    })
-
-    onBeforeUpdate(() => {
-      console.log('onBeforeUpdate')
-    })
-
-    onUpdated(() => {
-      console.log('onUpdated')
-    })
-
-
-
-    // console.log(isReactive(framework)) //true
-    // console.log(isReactive(framework.name)) //false
-
-    // const doubleVersion = computed(() => version.value * 2)
-
-    // watch([doubleVersion, name], (newValues, oldValues) => {
-    //   console.log('new', newValues, 'old', oldValues) // new [8, 'Vue JS!'] old [6, 'VueJS']
-    // })
-
-    watch(firstName, (newValue, oldValue) => {
-      // console.log(newValue) // input.value
-    })
-
-    function changeInfo() {
-      name.value = 'Vue JS!'
-      version.value = 4
-
-      // console.log(textInput.value) // input element
-    }
-
-    const changeVersion = (num) => {
-      version.value = num
-    }
-
-    provide('name', name)
-    provide('version', version)
-
-    return {
-      change: changeInfo,
-      textInput,
-      firstName,
-      changeVersion,
-    }
-  },
-  components: {
-    FrameworkInfo
-  }
 }
 </script>
